b5bec1bf94680b1a802dbda419597bef80ded624
Lucas Urbanski <lucas.urbanski@edu.sait.ca>
2026-01-20 14:45:55 -0700

    week-2

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+export default function StudentInfo() {
+  return (
+    <main>
+      <h1>Lucas Urbanski</h1>
+      <Link href={"https://github.com/Lucas-Urbanski/cprg306-assignments.git"}>
+        My Github Link
+      </Link>
+    </main>
+  );
+}
